16e58f63fd8ac573753fef95008b00311ad0810c
Fourier2718281828 <non-linear@ukr.net>
2024-02-14 00:44:24 +0200

    add basic interfaces for AVDP-structure of upgrade system

---
+++ Assets/Scripts/Upgrades/IUpgradeAbstractVisitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App.Upgrades
+{
+    public interface IUpgradeAbstractVisitor
+    {
+        sealed void Enable(IUpgradable upgradable)
+        {
+            var guide = @$"
+                AVDP Guide:
+                Do not call {nameof(IUpgradeAbstractVisitor)}::Enable({nameof(IUpgradable)} upgradable).
+                Instead use a dynamic_cast to concrete type implementing {nameof(IUpgradeAbstractVisitor)}, like 
+                {typeof(IUpgradeVisitor<>).Name}, and call its Enable method instead to perform a double-dispatch 
+                over upgrades.
+            ";
+
+            throw new InvalidOperationException(guide);
+        }
+
+        void Disable();
+        void LevelUp();
+        bool IsComplete { get; }
+        bool IsEnabled { get; }
+    }
+}
