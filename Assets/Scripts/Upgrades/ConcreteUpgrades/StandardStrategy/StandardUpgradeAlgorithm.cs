ebd6ecee73b2da75b99f95c177fbb8ef053288b2
Fourier2718281828 <non-linear@ukr.net>
2024-02-14 01:05:58 +0200

    add upgrade strategy interface; add standard upgrade algorithm (level management); add standard upgrade scriptable object base

---
+++ Assets/Scripts/Upgrades/ConcreteUpgrades/StandardStrategy/StandardUpgradeAlgorithm.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace App.Upgrades.ConcreteUpgrades.StandardStrategy
+{
+    public class StandardUpgradeAlgorithm<UpgradableEntity, LevelType> : IUpgradeVisitor<UpgradableEntity>
+
+        where UpgradableEntity : class, IUpgradable
+    {
+        private readonly UpgradeLevelManager<LevelType> levelManager;
+        private readonly IStrategy<UpgradableEntity, LevelType> strategy;
+        private UpgradableEntity upgradableEntity;
+
+        public bool IsComplete => levelManager.IsComplete;
+        public bool IsEnabled => (upgradableEntity != null);
+        public UpgradableEntity CurrentUpgradableEntity => upgradableEntity;
+
+        public StandardUpgradeAlgorithm(IStrategy<UpgradableEntity, LevelType> strategy, List<LevelType> levelSet)
+        {
+            this.strategy = strategy;
+            levelManager = new(levelSet);
+        }
+
+        public void Disable()
+        {
+            strategy.Reset(upgradableEntity);
+            upgradableEntity = null;
+        }
+
+        public void Enable(UpgradableEntity upgradable)
+        {
+            upgradableEntity = upgradable;
+            strategy.Initialize(upgradableEntity);
+            strategy.SwitchToLevel(upgradableEntity, levelManager.CurrentLevel);
+        }
+
+        public void LevelUp()
+        {
+            levelManager.LevelUp();
+
+            if (IsEnabled)
+            {
+                strategy.SwitchToLevel(upgradableEntity, levelManager.CurrentLevel);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Upgrades/ConcreteUpgrades/StandardStrategy/StandardUpgradeScriptableObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Upgrades.ConcreteUpgrades.StandardStrategy
+{
+    public class StandardUpgradeScriptableObject<UpgradableEntity, LevelType> : BaseUpgradeScriptableObject<UpgradableEntity>
+
+        where UpgradableEntity : class, IUpgradable
+    {
+        #region Serialized Fields
+        [SerializeField] private List<LevelType> levels;
+        #endregion
+
+        #region Fields
+        private readonly StandardUpgradeAlgorithm<UpgradableEntity, LevelType> upgradeAlgorithm;
+        #endregion
+
+        #region Properties
+        public override bool IsComplete => upgradeAlgorithm.IsComplete;
+        public override bool IsEnabled => upgradeAlgorithm.IsEnabled;
+        #endregion
+
+        #region Constructors
+        public StandardUpgradeScriptableObject(IStrategy<UpgradableEntity, LevelType> strategy)
+        {
+            levels = new();
+            upgradeAlgorithm = new(strategy, levels);
+        }
+        #endregion
+
+        #region Methods
+        public override void Disable()
+        {
+            upgradeAlgorithm.Disable();
+        }
+
+        public override void Enable(UpgradableEntity upgradable)
+        {
+            upgradeAlgorithm.Enable(upgradable);
+        }
+
+        public override void LevelUp()
+        {
+            upgradeAlgorithm.LevelUp();
+        }
+        #endregion
+    }
+}
