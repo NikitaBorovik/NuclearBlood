e7c1ffd74852b390ba8645ba6e0fab9f67aeff16
Fourier2718281828 <non-linear@ukr.net>
2024-02-14 00:53:35 +0200

    add displayable upgrade interface; add base for upgrade scriptable objects

---
+++ Assets/Scripts/Upgrades/IDisplayableUpgrade.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace App.Upgrades
+{
+    public interface IDisplayableUpgrade
+    {
+        Sprite Image { get; }
+        string Name { get; }
+        string Description { get; }
+    }
+}
