ce2e95ae91b3215e0667ed4e46a481024dcd9af8
Fourier2718281828 <non-linear@ukr.net>
2024-02-23 04:19:34 +0200

    added: Accuracy upgrade

---
+++ Assets/Scripts/Upgrades/ConcreteUpgrades/StandardStrategy/PlayerUpgrades/AccuracyUpgrade.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using App.World.Entity.Player.PlayerComponents;
+using System;
+
+namespace App.Upgrades.ConcreteUpgrades.StandardStrategy.PlayerUpgrades
+{
+    [System.Serializable]
+    public struct AccuracyUpgradeLevel
+    {
+        [Range(0f, 1f)] public float accuracy;
+    }
+
+    [CreateAssetMenu(fileName = "Accuracy", menuName = "Scriptable Objects/Upgrades/AccuracyUpgrade")]
+    public class AccuracyUpgrade : StandardUpgradeScriptableObject<Player, AccuracyUpgradeLevel>
+    {
+        public AccuracyUpgrade() : base(new PlayerAccuracyUpgradeStrategy()) { }
+    }
+
+    public class PlayerAccuracyUpgradeStrategy : IStrategy<Player, AccuracyUpgradeLevel>
+    {
+        private float? initialBulletSpread = null;
+
+        public void Initialize(Player player)
+        {
+            initialBulletSpread = player.Weapon.BulletFlySpeed;
+        }
+
+        public void Destroy()
+        {
+            initialBulletSpread = null;
+        }
+
+        public void Reset(Player player)
+        {
+            player.Weapon.BulletFlySpeed = initialBulletSpread
+                ?? throw new InvalidOperationException("Cannot reset via a non-initialized strategy");
+        }
+
+        public void SwitchToLevel(Player player, AccuracyUpgradeLevel level)
+        {
+            Reset(player);
+            player.Weapon.BulletFlySpeed *= (1 - level.accuracy);
+        }
+    }
+}
