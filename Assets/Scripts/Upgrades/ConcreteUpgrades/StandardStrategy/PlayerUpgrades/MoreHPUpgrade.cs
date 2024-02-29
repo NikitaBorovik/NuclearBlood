4557e582cfca23fa15efcae305fab73cf6107a38
Fourier2718281828 <non-linear@ukr.net>
2024-02-29 23:24:56 +0200

    added: more hp upgrade

---
+++ Assets/Scripts/Upgrades/ConcreteUpgrades/StandardStrategy/PlayerUpgrades/MoreHPUpgrade.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using App.World.Entity.Player.PlayerComponents;
+using System;
+
+namespace App.Upgrades.ConcreteUpgrades.StandardStrategy.PlayerUpgrades
+{
+    [System.Serializable]
+    public struct MoreHPUpgradeLevel
+    {
+        [Min(0)] public float maxHP;
+    }
+
+    [CreateAssetMenu(fileName = "MoreHP", menuName = "Scriptable Objects/Upgrades/MoreHPUpgrade")]
+    public class MoreHPUpgrade : StandardUpgradeScriptableObject<Player, MoreHPUpgradeLevel>
+    {
+        public MoreHPUpgrade() : base(new PlayerMoreHPUpgradeStrategy()) { }
+    }
+
+    public class PlayerMoreHPUpgradeStrategy : IStrategy<Player, MoreHPUpgradeLevel>
+    {
+        private float? initialMaxHP = null;
+
+        public void Initialize(Player player)
+        {
+            initialMaxHP = player.Health.MaxHealth;
+        }
+
+        public void Reset(Player player)
+        {
+            player.Health.MaxHealth = initialMaxHP
+                ?? throw new InvalidOperationException("Cannot reset via a non-initialized strategy");
+        }
+
+        public void SwitchToLevel(Player entity, MoreHPUpgradeLevel level)
+        {
+            Reset(entity);
+            entity.Health.MaxHealth = level.maxHP;
+        }
+    }
+}
