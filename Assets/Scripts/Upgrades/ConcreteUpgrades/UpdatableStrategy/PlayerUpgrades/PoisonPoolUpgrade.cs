6847e48ce08b4e1851a7e68b1f1c8bfe05e3ae14
Fourier2718281828 <non-linear@ukr.net>
2024-03-15 09:22:01 +0200

    added: poison pool upgrade

---
+++ Assets/Scripts/Upgrades/ConcreteUpgrades/UpdatableStrategy/PlayerUpgrades/PoisonPoolUpgrade.cs
@@ -0,0 +1,61 @@
+﻿using App.World.Entity.Player.PlayerComponents;
+using UnityEngine;
+
+namespace App.Upgrades.ConcreteUpgrades.UpdatableStrategy.PlayerUpgrades
+{
+    [System.Serializable]
+    public struct PoisonPoolUpgradeLevel
+    {
+        [Min(0.1f)] public float damage;
+        [Min(1)] public int hitNumber;
+        [Range(0.05f, 5f)] public float hitPeriod;
+        [Range(1f, 30f)] public float appearancePeriod;
+        [Range(1f, 30f)] public float existanceDuration;
+        public GameObject poisonPoolPrefab;
+    }
+
+    [CreateAssetMenu(fileName = "PoisonPool", menuName = "Scriptable Objects/Upgrades/PoisonPoolUpgrade")]
+    public class PoisonPoolUpgrade : StandardUpdatableUpgradeScriptableObject<Player, PoisonPoolUpgradeLevel>
+    {
+        public PoisonPoolUpgrade() : base(new PoisonPoolUpgradeUpgradeStrategy()) { }
+    }
+
+    public class PoisonPoolUpgradeUpgradeStrategy : IUpdatableStrategy<Player, PoisonPoolUpgradeLevel>
+    {
+        PoisonPoolUpgradeLevel currentLevel;
+        private float timeCounter { get; set; } = 0.0f;
+
+        public void Initialize(Player player) { }
+
+        public void Reset(Player player) { }
+
+        public void SwitchToLevel(Player player, PoisonPoolUpgradeLevel level)
+        {
+            currentLevel = level;
+        }
+
+        public void Update(Player upgradableEntity)
+        {
+            if (timeCounter > currentLevel.appearancePeriod)
+            {
+                SpawnPoisonPool(upgradableEntity.transform.position);
+                timeCounter = 0f;
+            }
+            else
+            {
+                timeCounter += Time.deltaTime;
+            }
+        }
+
+        private void SpawnPoisonPool(Vector3 position)
+        {
+            GameObject poisonPoolPrefab = currentLevel.poisonPoolPrefab;
+            GameObject instantiated = GameObject.Instantiate(poisonPoolPrefab, position, Quaternion.identity);
+            PoisonPool poisonPoolComponent = instantiated.GetComponent<PoisonPool>();
+            poisonPoolComponent.Init(currentLevel.hitPeriod,
+                                     currentLevel.damage,
+                                     currentLevel.hitNumber,
+                                     currentLevel.existanceDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/PoisonPool.cs
@@ -0,0 +1,90 @@
+﻿using App.World.Entity;
+using App.World.Entity.Enemy;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonPool : MonoBehaviour
+{
+    private float? hitPeriod = null;
+    private float? damage = null;
+    private int? hitNumber = null;
+    private float? existenceDuration = null;
+
+    private List<Health> poisonedHealths = new();
+    private float timeCounter = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!gameObject.activeSelf)
+        {
+            return;
+        }
+
+        if (!IsInitialized())
+        {
+            return;
+        }
+
+        var enemyComponent = collision.gameObject.GetComponent<BaseEnemy>();
+
+        if (enemyComponent == null)
+        {
+            return;
+        }
+
+        var health = enemyComponent.Health;
+
+        if (gameObject.activeSelf && !IsPoisoned(health))
+        {
+            poisonedHealths.Add(health);
+            StartCoroutine(ApplyPoisoningEffect(health));
+        }
+        else
+        {
+            poisonedHealths.Remove(health);
+        }
+    }
+
+    private void Update()
+    {
+        if (!IsInitialized())
+        {
+            return;
+        }
+
+        if (timeCounter > existenceDuration)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            timeCounter += Time.deltaTime;
+        }
+    }
+
+    public void Init(float hitPeriod, float damage, int hitNumber, float existenceDuration)
+    {
+        this.hitPeriod = hitPeriod;
+        this.damage = damage;
+        this.hitNumber = hitNumber;
+        this.existenceDuration = existenceDuration;
+    }
+
+    private IEnumerator ApplyPoisoningEffect(Health health)
+    {
+        for (int i = 0; i < hitNumber; ++i)
+        {
+            Debug.Log("Hit");
+            health.TakeDamage(damage.Value);
+            yield return new WaitForSeconds(hitPeriod.Value);
+        }
+
+        poisonedHealths.Remove(health);
+    }
+
+    private bool IsPoisoned(Health health) => poisonedHealths.Find(h => h == health) != null;
+
+    private bool IsInitialized()
+        => hitPeriod != null && damage != null && hitNumber != null && existenceDuration != null;
+}
