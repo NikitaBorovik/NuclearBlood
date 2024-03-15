330a7a1e20fa48c2289c833243c48dd8520eb274
Fourier2718281828 <non-linear@ukr.net>
2024-03-15 09:18:16 +0200

    added: HPRegenRateUpgrade

---
+++ Assets/Scripts/Upgrades/ConcreteUpgrades/UpdatableStrategy/PlayerUpgrades/HPRegenRateUpgrade.cs
@@ -0,0 +1,49 @@
+﻿using App.World.Entity.Player.PlayerComponents;
+using UnityEngine;
+
+namespace App.Upgrades.ConcreteUpgrades.UpdatableStrategy.PlayerUpgrades
+{
+    [System.Serializable]
+    public struct HpRegenRateUpgradeLevel
+    {
+        [Min(0.0f)] public float hpRegenRateAddent;
+        [Min(0.0f)] public float period;
+    }
+
+    [CreateAssetMenu(fileName = "HPRegenRate", menuName = "Scriptable Objects/Upgrades/HPRegenRateUpgrade")]
+    public class HPRegenRateUpgrade : StandardUpdatableUpgradeScriptableObject<Player, HpRegenRateUpgradeLevel>
+    {
+        public HPRegenRateUpgrade() : base(new HPRegenRateUpgradeUpgradeStrategy()) { }
+    }
+
+    public class HPRegenRateUpgradeUpgradeStrategy : IUpdatableStrategy<Player, HpRegenRateUpgradeLevel>
+    {
+        private float hpRegenRateAddent { get; set; }
+        private float period { get; set; }
+        private float timeCounter { get; set; } = 0.0f;
+
+        public void Initialize(Player player) { }
+
+        public void Reset(Player player) { }
+
+        public void SwitchToLevel(Player player, HpRegenRateUpgradeLevel level)
+        {
+            hpRegenRateAddent = level.hpRegenRateAddent;
+            period = level.period;
+        }
+
+        public void Update(Player upgradableEntity)
+        {
+            var health = upgradableEntity.Health;
+            if (timeCounter > period)
+            {
+                health.Heal(hpRegenRateAddent);
+                timeCounter = 0f;
+            }
+            else
+            {
+                timeCounter += Time.deltaTime;
+            }
+        }
+    }
+}
